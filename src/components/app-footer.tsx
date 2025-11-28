d1c8acb813c54f8e19dfead610d3ed234d039f91
Raveen Singh <raveenone@gmail.com>
2025-11-28 21:27:12 +0530

    Updating light mode colors to dark mode as well.

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function AppFooter() {
   return (
-    <footer className="text-center p-2 bg-neutral-100 dark:bg-[#0E0017] dark:text-neutral-400 text-xs">
+    <footer className="text-center p-2 bg-[#0E0017] dark:bg-[#0E0017] dark:text-neutral-400 text-xs">
       Satoshi Strike Force{' '}
     </footer>
   )
