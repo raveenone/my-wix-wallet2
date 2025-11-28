441e40dc38c3418ff2ae4f7407f899bf17b602b2
Raveen Singh <raveenone@gmail.com>
2025-11-28 19:53:38 +0530

    Added SSF references

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -11,24 +11,7 @@ const links: { label: string; href: string }[] = [
 export function DashboardFeature() {
   return (
     <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana app." />
-      <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
-        <div className="space-y-2">
-          <p>Here are some helpful links to get you started.</p>
-          {links.map((link, index) => (
-            <div key={index}>
-              <a
-                href={link.href}
-                className="hover:text-gray-500 dark:hover:text-gray-300"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.label}
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>
+      <AppHero title="Satoshi Strike Force" subtitle="Token on solana" />
     </div>
   )
 }
